2ac997305f38330b9de1c1062845187c279a7b4b
Labrynther <labrynthergd@gmail.com>
2026-05-22 22:45:44 -0400

    Update lexer.c

--- lexer.c
+++ lexer.c
@@ -1,37 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
 #include "share.h"
+#include "keywordHash.h"
 
-LexerState* init_lexer(const char* fileContent, int fileSize) {
-    LexerState* lexer = malloc(sizeof(LexerState));
-    if (lexer == NULL) {
-        fprintf(stderr, "\033[31mUnable to Initalize Lexer");
-        exit(1);
-    }
+#define ORIG_TOKEN_CAPACITY 512
 
-    lexer->file = fileContent;
-    lexer->fileSize = fileSize;
-    lexer->position = 0;
-    lexer->currentLine = 1;
-    lexer->currentColumn = 1;
-    lexer->capacity = ORIG_TOKEN_CAPACITY;
-    lexer->count = 0;
+LexerState* initLexer(char* filename, char* fileContent, size_t fileSize) {
+    LexerState* initLex = allocate(sizeof(LexerState));
 
-    lexer->tokenArray = malloc(sizeof(Token) * lexer->capacity);
-    if (lexer->tokenArray == NULL) {
-        fprintf(stderr, "\033[31mFailed to allocate token array\n");
-        exit(1);
-    }
+    initLex->filename = filename;
+    initLex->file = fileContent;
+    initLex->fileSize = fileSize;
+    initLex->position = 0;
+    initLex->currentLine = 1;
+    initLex->currentColumn = 1;
+    initLex->capacity = ORIG_TOKEN_CAPACITY;
+    initLex->count = 0;
 
-    return lexer;
+    initLex->tokenArray = allocate(sizeof(Token) * initLex->capacity);
+
+    return initLex;
 }
 
-void changePosition(LexerState* lexer){
-    if(lexer->position < lexer->fileSize){
-        if (lexer->file[lexer->position] == '\n') {
+char charPeek(size_t n, LexerState *lexer) {
+    if(lexer->position + n < lexer->fileSize) 
+        return lexer->file[lexer->position + n];
+
+    return '\0';
+}
+
+void changePosition(LexerState* lexer) {
+    if(lexer->currentChar) {
+        if (lexer->currentChar == '\n') {
             lexer->currentLine++;
             lexer->currentColumn = 1;
         } else {
@@ -39,24 +40,20 @@ void changePosition(LexerState* lexer){
         }
         lexer->position++;
     }
+    lexer->currentChar = charPeek(0, lexer);
 }
 
-Token generateToken(LexerState* lexer, TokenType type, TokenLocation startPos){
+Token generateToken(TokenType type, DataType state, TokenLocation startPos, LexerState *lexer) {
     if (lexer->count == lexer->capacity) {
         int newCapacity = lexer->capacity + ORIG_TOKEN_CAPACITY;
-        Token *temp = realloc(lexer->tokenArray, newCapacity * sizeof(Token));
 
-        if (temp == NULL) {
-            fprintf(stderr, "Out of memory!\n");
-            exit(1); 
-        }
-
-        lexer->tokenArray = temp;
+        lexer->tokenArray = reallocate(lexer->tokenArray, newCapacity * sizeof(Token));
         lexer->capacity = newCapacity;
     }
 
     Token lexeme = {
         .type = type,
+        .state = state,
         .pos.loc = startPos.loc,
         .pos.line = startPos.line,
         .pos.column = startPos.column,
@@ -69,12 +66,12 @@ Token generateToken(LexerState* lexer, TokenType type, TokenLocation startPos){
     return lexeme;
 }
 
-Token scanToken(LexerState* lexer){
+Token scanToken(LexerState* lexer) {
     continueScan:
 
-    while (lexer->position < lexer->fileSize && isspace(lexer->file[lexer->position])){
-        changePosition(lexer);
-    }
+    while (isspace((unsigned char)charPeek(0, lexer))) changePosition(lexer);
+
+    lexer->currentChar = charPeek(0, lexer);
 
     TokenLocation startPos = {
         .line = lexer->currentLine,
@@ -82,45 +79,33 @@ Token scanToken(LexerState* lexer){
         .loc = lexer->position,
     };
 
-    if(lexer->position >= lexer->fileSize){
-        return generateToken(lexer, TOKEN_EOF, startPos);
-    }
+    if(lexer->position >= lexer->fileSize) return generateToken(TOKEN_EOF, NONE, startPos, lexer);
 
-    switch(lexer->file[lexer->position]){
+    switch(lexer->currentChar) {
         case '"':
             changePosition(lexer);
-            while (lexer->file[lexer->position] != '"'){
-                if(lexer->position >= lexer->fileSize){
-                    throwError("Unterminated String", startPos.line, startPos.column);
+            while (lexer->currentChar != '"') {
+                if(lexer->position >= lexer->fileSize) {
+                    throwError(lexer->filename, "Unterminated String\n", startPos.line, startPos.column, 1, ERROR_UNTERMINATED_STRING, ERROR);
+                    return generateToken(TOKEN_LITERAL, LITERAL_STRING, startPos, lexer);
                 }
                 changePosition(lexer);
             }
             changePosition(lexer);
-            return generateToken(lexer, TOKEN_STRING, startPos);
-
-        case '\'':
-            changePosition(lexer);
-            while (lexer->file[lexer->position] != '\''){
-                if(lexer->position >= lexer->fileSize){
-                    throwError("Unterminated Character Literal", startPos.line, startPos.column);
-                }
-                changePosition(lexer);
-            }
-            changePosition(lexer);
-            return generateToken(lexer, TOKEN_CHAR, startPos);
+            return generateToken(TOKEN_LITERAL, LITERAL_STRING, startPos, lexer);
 
         case '/':
             // Handle Comments
-            if(lexer->file[lexer->position+1] == '/') {
-                while(lexer->file[lexer->position] != '\n' && lexer->position < lexer->fileSize){
+            if(charPeek(1, lexer) == '/') {
+                while(lexer->currentChar != '\n') {
                     changePosition(lexer);
                 }
                 goto continueScan;
-            } else if (lexer->position + 1 < lexer->fileSize && lexer->file[lexer->position+1] == '*') {
+            } else if (charPeek(1, lexer) == '*') {
                 changePosition(lexer);
                 changePosition(lexer);
                 while(lexer->position < lexer->fileSize) {
-                    if (lexer->position + 1 < lexer->fileSize && lexer->file[lexer->position] == '*' && lexer->file[lexer->position+1] == '/'){
+                    if (lexer->currentChar == '*' && charPeek(1, lexer) == '/') {
                         changePosition(lexer);
                         changePosition(lexer);
                         goto continueScan;
@@ -128,103 +113,200 @@ Token scanToken(LexerState* lexer){
                     changePosition(lexer);
                 }
 
-                throwError("Unterminated block comment", startPos.line, startPos.column);
+                throwError(lexer->filename, "Unterminated block comment", startPos.line, startPos.column, 2, ERROR_UNTERMINATED_BLOCK_COMMENT, ERROR);
+                while(lexer->position < lexer->fileSize) changePosition(lexer);
+                goto continueScan;
             }
 
             changePosition(lexer);
-            return generateToken(lexer, TOKEN_DIVIDE, startPos);
+            return generateToken(TOKEN_OP, OP_DIVISION, startPos, lexer);
 
-                // general symbol cases
-                
-        case ';': changePosition(lexer); return generateToken(lexer, TOKEN_SEMICOLON, startPos);
-        case ':': changePosition(lexer); return generateToken(lexer, TOKEN_COLON, startPos);
-        case ',': changePosition(lexer); return generateToken(lexer, TOKEN_COMMA, startPos);
-        case '(': changePosition(lexer); return generateToken(lexer, TOKEN_LPAREN, startPos);
-        case ')': changePosition(lexer); return generateToken(lexer, TOKEN_RPAREN, startPos);
-        case '{': changePosition(lexer); return generateToken(lexer, TOKEN_LBRACE, startPos);
-        case '}': changePosition(lexer); return generateToken(lexer, TOKEN_RBRACE, startPos);
-        case '.': changePosition(lexer); return generateToken(lexer, TOKEN_PERIOD, startPos);
-        case '*': changePosition(lexer); return generateToken(lexer, TOKEN_MULTIPLY, startPos);
-        case '+': changePosition(lexer); return generateToken(lexer, TOKEN_PLUS, startPos);
-        case '-': changePosition(lexer); return generateToken(lexer, TOKEN_MINUS, startPos);
-
-        case '=': 
-            if(lexer->file[lexer->position+1] == '=') { changePosition(lexer); changePosition(lexer); return generateToken(lexer, TOKEN_ASSIGN, startPos); }
+        case '\'':
             changePosition(lexer);
-            return generateToken(lexer, TOKEN_EQUAL, startPos);
+            while (lexer->currentChar != '\'') {
+                if(lexer->position >= lexer->fileSize) {
+                    throwError(lexer->filename, "Unterminated Character Literal\n", startPos.line, startPos.column, 1, ERROR_UNTERMINATED_CHARACTER_LITERAL, ERROR);
+                    return generateToken(TOKEN_LITERAL, LITERAL_CHAR, startPos, lexer);
+                }
+                changePosition(lexer);
+            }
+            changePosition(lexer);
+            return generateToken(TOKEN_LITERAL, LITERAL_CHAR, startPos, lexer);
 
-        case '[': changePosition(lexer); return generateToken(lexer, TOKEN_LBRACKET, startPos);
-        case ']': changePosition(lexer); return generateToken(lexer, TOKEN_RBRACKET, startPos);
-        case '>': changePosition(lexer); return generateToken(lexer, TOKEN_GREATER, startPos);
-        case '<': changePosition(lexer); return generateToken(lexer, TOKEN_LESS, startPos);
-        case '%': changePosition(lexer); return generateToken(lexer, TOKEN_PERCENT, startPos);
-        case '?': changePosition(lexer); return generateToken(lexer, TOKEN_QMARK, startPos);
-        case '&': changePosition(lexer); return generateToken(lexer, TOKEN_AMPERSAND, startPos);
+        case '.':
+            if(charPeek(1, lexer) == '.' && charPeek(2, lexer) == '.') { changePosition(lexer); changePosition(lexer); changePosition(lexer);
+                return generateToken(TOKEN_ELLIPSIS, NONE, startPos, lexer); }
+
+            changePosition(lexer);
+            return generateToken(TOKEN_DOT, NONE, startPos, lexer);
+
+        case '<':
+            if(charPeek(1, lexer) == '-') {
+                changePosition(lexer);
+                changePosition(lexer);
+                return generateToken(TOKEN_PIPE, LEFT, startPos, lexer);
+            }
+            changePosition(lexer);
+            return generateToken(TOKEN_ANGLE, LEFT, startPos, lexer);
+        
+        case '-':
+            if(charPeek(1, lexer) == '>') {
+                changePosition(lexer);
+                changePosition(lexer);
+                return generateToken(TOKEN_PIPE, LEFT, startPos, lexer);
+            }
+
+            if(isdigit(charPeek(1, lexer))) {
+                changePosition(lexer);
+                goto lexNumber;
+            }
+
+            changePosition(lexer);
+            return generateToken(TOKEN_OP, OP_MINUS, startPos, lexer);
+        
+        case '*': changePosition(lexer); return generateToken(TOKEN_OP, OP_MULTIPLICATION, startPos, lexer);
+        case '+': changePosition(lexer); return generateToken(TOKEN_OP, OP_PLUS, startPos, lexer);
+        case '=': changePosition(lexer); return generateToken(TOKEN_EQUAL, NONE, startPos, lexer);
+        case ';': changePosition(lexer); return generateToken(TOKEN_SEMICOLON, NONE, startPos, lexer);
+        case ':': changePosition(lexer); return generateToken(TOKEN_COLON, NONE, startPos, lexer);
+        case ',': changePosition(lexer); return generateToken(TOKEN_COMMA, NONE, startPos, lexer);
+        case '(': changePosition(lexer); return generateToken(TOKEN_PAREN, NONE, startPos, lexer);
+        case ')': changePosition(lexer); return generateToken(TOKEN_PAREN, NONE, startPos, lexer);
+        case '{': changePosition(lexer); return generateToken(TOKEN_BRACE, LEFT, startPos, lexer);
+        case '}': changePosition(lexer); return generateToken(TOKEN_BRACE, RIGHT, startPos, lexer);
+        case '[': changePosition(lexer); return generateToken(TOKEN_BRACKET, LEFT, startPos, lexer);
+        case ']': changePosition(lexer); return generateToken(TOKEN_BRACKET, RIGHT, startPos, lexer);
+        case '>': changePosition(lexer); return generateToken(TOKEN_ANGLE, RIGHT, startPos, lexer);
+        case '%': changePosition(lexer); return generateToken(TOKEN_OP, OP_MODULO, startPos, lexer);
+        case '?': changePosition(lexer); return generateToken(TOKEN_QMARK, NONE, startPos, lexer);
+        case '&': changePosition(lexer); return generateToken(TOKEN_AMPERSAND, NONE, startPos, lexer);
 
         default:
-            if (isalpha(lexer->file[lexer->position]) || lexer->file[lexer->position] == '_') {
-                while(isalnum(lexer->file[lexer->position]) || lexer->file[lexer->position] == '_') {
+            if (isalpha(lexer->currentChar) || lexer->currentChar == '_') {
+                while(isalnum(lexer->currentChar) || lexer->currentChar == '_') {
                     changePosition(lexer);
                 }
-                return generateToken(lexer, TOKEN_IDENTIFIER, startPos);
-            } else if (isdigit(lexer->file[lexer->position])){
-                while(isdigit(lexer->file[lexer->position])) {
-                    changePosition(lexer);
+
+                const struct keyword *k = in_word_set(&lexer->file[startPos.loc], lexer->position - startPos.loc);
+
+                if(k) return generateToken(TOKEN_IDENTIFIER, k->state, startPos, lexer);
+
+                return generateToken(TOKEN_IDENTIFIER, NONE, startPos, lexer);
+
+            } else if (isdigit((unsigned char)lexer->currentChar)) {
+                lexNumber: ;
+                bool seen_dot = false;
+
+                while(lexer->currentChar != '\n') {
+                    if (isdigit((unsigned char)lexer->currentChar)) {
+                        changePosition(lexer);
+                        continue;
+                    }
+
+                    if (lexer->currentChar == '.' && !seen_dot && isdigit((unsigned char)charPeek(1, lexer))) {
+                        seen_dot = true;
+                        changePosition(lexer); // consume '.'
+                        continue;
+                    }
+                    break;
                 }
-                return generateToken(lexer, TOKEN_NUMBER, startPos);
+
+                return generateToken(TOKEN_LITERAL, LITERAL_NUMBER, startPos, lexer);
             } else {
                 char unknownChar = lexer->file[lexer->position];
-                char unknownCharMessageBuffer[28];
-                snprintf(unknownCharMessageBuffer, sizeof(unknownCharMessageBuffer), "Unknown Character called: %c", unknownChar);
-                throwError(unknownCharMessageBuffer, startPos.line, startPos.column);
-                return generateToken(lexer, TOKEN_UNKNOWN, startPos);
+                char buffer[64];
+                snprintf(buffer, sizeof(buffer), "Character '%c' is not known or allowed\n", unknownChar);
+                throwError(lexer->filename, buffer, startPos.line, startPos.column, 1, ERROR_UNKNOWN_CHARACTER, ERROR);
+                changePosition(lexer);
+
+                return generateToken(TOKEN_UNKNOWN, NONE, startPos, lexer);
             }
     }
 }
+
 
 void showLex(LexerState* lexer){
     const char *tokenTypeLabels[] = {
         "IDENTIFIER",
-        "STRING",
-        "CHAR", 
-        "LBRACE", 
-        "RBRACE", 
-        "LPAREN", 
-        "RPAREN", 
-        "LBRACKET", 
-        "RBRACKET", 
+        "LITERAL",
+        "TYPE",
+
+        "PIPE",
+        "DOT",
+        "COMMA",
+        "COLON",
         "SEMICOLON",
-        "COLON", 
-        "COMMA", 
-        "PERIOD",
-        "NUMBER",
-        "PLUS", 
-        "MINUS", 
-        "MULTIPLY",
-        "DIVIDE", 
+
+        "BRACE",
+        "PAREN",
+        "BRACKET",
+        "ANGLE",
+
+        "OP",
+
         "EQUAL",
-        "GREATER",
-        "LESS",
-        "PERCENT",
         "QMARK",
         "AMPERSAND",
-        "ASSIGNMENT",
+        "ELLIPSIS",
+
+        "ARROW",
+
         "EOF",
         "UNKNOWN"
     };
-    
-    printf("\033[1;34mShowing File Contents:\n\n");
+
+    const char *dataTypeLabels[] = {
+        "NONE",
+        "OBJECT",
+        "IMPORT",
+        "RETURN",
+
+        "INT",
+        "CHAR",
+        "FLOAT",
+        "DOUBLE",
+        "BOOL",
+        "CONST",
+        "POINTER",
+        "PRIVATE",
+
+        "NUMBER",
+        "STRING",
+        "CHAR",
+
+        "PLUS",
+        "MINUS",
+        "MULTIPLICATION",
+        "DIVISION",
+        "MODULO",
+
+        "LEFT",
+        "RIGHT"
+    };
+
+    printf("\033[1;34mShowing File Contents:\n\n\033[0m");
     for(int i = 0; i < lexer->fileSize; i++){
         printf("\033[0m%c", lexer->file[i]);
     }
+    printf("\033[0m");
+
     printf("\n\n\033[1;34mShowing Generated Tokens:\n");
     for(int i = 0; i < lexer->count; i++){
-        printf("\n\033[0mType: %s\nLocation: %d\nLength: %d\nLine: %d\nColumn: %d\n", tokenTypeLabels[lexer->tokenArray[i].type], lexer->tokenArray[i].pos.loc, lexer->tokenArray[i].length, lexer->tokenArray[i].pos.line, lexer->tokenArray[i].pos.column);
+        printf("\n\033[0mType: %s\nINFO: %s\nLocation: %zu\nLength: %d\nLine: %u\nColumn: %u\n",
+                tokenTypeLabels[lexer->tokenArray[i].type],
+                dataTypeLabels[lexer->tokenArray[i].state],
+                lexer->tokenArray[i].pos.loc,
+                lexer->tokenArray[i].length,
+                lexer->tokenArray[i].pos.line,
+                lexer->tokenArray[i].pos.column);
     }
+    printf("\033[0m");
 }
 
-int lex(LexerState* lexer){
-    while(lexer->position < lexer->fileSize){
+int lex(File *file) {
+    LexerState *lexer = file->lexer;
+
+    while(1) {
         Token eofCheck = scanToken(lexer);
         if (eofCheck.type == TOKEN_EOF) break;
     }
