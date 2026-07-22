198e28b027fab7ab46089908c3fe82346da6a81b
Labrynther <labrynthergd@gmail.com>
2026-07-22 18:06:10 -0400

    Refactor share.h with new structures and includes

--- share.h
+++ share.h
@@ -1,57 +1,17 @@
-#ifndef TOKENS_H
-#define TOKENS_H
+#ifndef SHARE_H
+#define SHARE_H
 
-#define ORIG_TOKEN_CAPACITY 512
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdatomic.h>
+#include <string.h>
 
-typedef enum {
-    // Special Cases
-    TOKEN_IDENTIFIER, 
-    TOKEN_STRING,
-    TOKEN_CHAR, 
-    // Braces/Parentheses/Brackets
-    TOKEN_LBRACE, 
-    TOKEN_RBRACE, 
-    TOKEN_LPAREN, 
-    TOKEN_RPAREN,
-    TOKEN_LBRACKET, 
-    TOKEN_RBRACKET, 
-    // Regular Symbols
-    TOKEN_SEMICOLON, 
-    TOKEN_COLON, 
-    TOKEN_COMMA, 
-    TOKEN_PERIOD,
-    // Arithmetic/Logic
-    TOKEN_NUMBER,
-    TOKEN_PLUS,
-    TOKEN_MINUS,
-    TOKEN_MULTIPLY,
-    TOKEN_DIVIDE,   
-    TOKEN_EQUAL,
-    TOKEN_GREATER,
-    TOKEN_LESS,
-    TOKEN_PERCENT,
-    TOKEN_QMARK,
-    TOKEN_AMPERSAND,
-    TOKEN_ASSIGN,
-    // Special Characters
-    TOKEN_EOF,
-    TOKEN_UNKNOWN
-} TokenType;
+#include "tokens.h"
 
 typedef struct {
-    size_t line;
-    size_t column;
-    size_t loc;
-} TokenLocation;
-
-typedef struct Token {
-    TokenType type;
-    TokenLocation pos;
-    int length;
-} Token;
-
-typedef struct {
-    const char* file;
+    char* filename;
+    char* file;
     size_t fileSize;
     size_t position;
     size_t currentLine;
@@ -60,41 +20,285 @@ typedef struct {
     Token* tokenArray;
     size_t capacity;
     size_t count;
+
+    char currentChar;
 } LexerState;
 
-typedef enum {
-    AST_START,
-    AST_NUMBER,
-    AST_BINARY_OP,
-    AST_END,
-    // add more lol
-} ASTNodeType;
+typedef struct NodePage {
+    char* nodesArray;
+    size_t capacity;
+    size_t offset;
+
+    struct NodePage* next;
+} NodePage;
 
 typedef struct {
-    ASTNodeType type;
+    NodePage* firstPage;
+    NodePage* currentPage;
+} NodePages;
 
+typedef enum {
+    AST_PROGRAM,
+
+    // Operations & Access
+    AST_MEMBER_ACCESS,
+    AST_BINARY_OP, // a + b, a - b, a * b, a / b
+    AST_UNARY_OP, // a++, a--
+    AST_INDEX,
+    AST_CAST,
+    AST_PIPE,
+
+    // Primitives
+    AST_IDENTIFIER,
+    AST_SPECIAL_IDENTIFIER,
+    AST_KEYWORD,
+    AST_NUMBER,
+    AST_STRING,
+    AST_CHAR,
+    AST_LIST,
+    AST_PROC,
+    AST_BUCKET,
+} ASTNodeType;
+
+typedef struct BucketNode BucketNode;
+
+typedef struct ASTNode {
+    ASTNodeType type;
+    Token mainToken;
+
+    BucketNode* buckets;
 } ASTNode;
 
 typedef struct {
+    ASTNode base;
+
+    Token* casts;
+    unsigned short castCount;
+
+    ASTNode* target;
+} CastNode;
+
+typedef struct BucketNode{
+    ASTNode base;
+
+    ASTNode** buckets;
+    short minimumBucketCount;
+    short maximumBucketCount;
+} BucketNode;
+
+typedef struct {
+    ASTNode base;
+    
+    ASTNode* left;
+    ASTNode* index;
+} IndexNode;
+
+typedef struct {
+    ASTNode base;
+
+    ASTNode* left;
+    Token property;
+} MemberAccessNode;
+
+typedef struct {
+    ASTNode base;
+
+    ASTNode* left;
+    ASTNode* right;
+} BinaryOpNode;
+
+typedef struct {
+    ASTNode base;
+
+    CastNode* casts;
+    bool special;
+} IdentifierNode;
+
+typedef struct {
+    ASTNode base;
+    ASTNode* name;
+
+    CastNode* casts;
+} KeywordNode;
+
+typedef struct {
+    ASTNode base;
+
+    CastNode* casts;
+} LiteralNode;
+
+typedef struct {
+    ASTNode base;
+
+    ASTNode** statements;
+    uint32_t statementCount;
+} ProcNode;
+
+typedef struct {
+    ASTNode base;
+
+    ASTNode** statements;
+    uint32_t statementCount;
+} ProgramNode;
+
+typedef struct {
+    ASTNode base;
+
+    ASTNode* target;
+} CompileTimeNode;
+
+typedef struct {
+    ASTNode base;
+    ASTNode* operand;
+    bool isPostfix;
+} UnaryOpNode;
+
+typedef struct {
+    ASTNode base;
+
+    CastNode* casts;
+    
+    ASTNode** elements;
+    uint32_t elementCount;
+} ListNode;
+
+typedef struct {
+    ASTNode base;
+
+    ASTNode** elements;
+    TokenType* pipeType;
+    StateType* directions;
+    TokenType* pipeTypes;
+    StateType* operations;
+
+    unsigned short elementsCount;
+} PipelineNode;
+
+typedef struct {
+    StateType type;
+    Token value;
+} Pragma;
+
+typedef struct {
+    Token* tokenArray;
+    int count;
+    int current;
+
+    NodePages* nodePages;
+
     char* filename;
+    int fileSize;
     const char* file;
+
+    // Used for things like Lists, Pipelines, etc...
+    ASTNode** elementBuffer;
+    unsigned short elementBufferCount;
+    unsigned short elementBufferCapacity;
+
+    // Used for Pipelines
+    StateType* directionBuffer;
+    unsigned short directionBufferCount;
+
+    TokenType* pipeTypeBuffer;
+    unsigned short pipeTypeBufferCount;
+
+    StateType* opBuffer;
+    unsigned short opBufferCount;
+
+    Token* castBuffer;
+    unsigned short castBufferCount;
+    unsigned short castBufferCapacity;
+
+    Pragma* pragmas;
+    unsigned short pragmaCount;
+    unsigned short pragmaCapacity;
+
+    ProgramNode* AST;
+
+    int depth;
+
+    bool hadError;
+} ParserState;
+
+typedef struct {
+    char* filename;
+    char* file;
     size_t fileSize;
     int fd;
+
     LexerState* lexer;
+    ParserState* parser;
 } File;
 
-LexerState* init_lexer(const char* fileContent, int fileSize);
-int lex(LexerState* lexer);
+typedef enum {
+    ERROR_UNTERMINATED,
+    ERROR_UNKNOWN_CHARACTER,
+    ERROR_EXPECTED,
+    ERROR_EMPTY,
+    ERROR_SYNTAX
+
+} ErrorType;
+
+typedef enum {
+    INFO,
+    WARNING,
+    ERROR
+} ErrorClass;
+
+typedef struct {
+    ErrorType errorType;
+    int info;
+
+    ErrorClass errorClass;
+
+    char* context;
+    char* filename;
+    const char* file;
+    int fileSize;
+
+    TokenLocation location;
+} Error;
+
+typedef struct {
+    Error* errorArray;
+
+    int errorCapacity;
+    int errorCount;
+} ErrorState;
+
+extern ErrorState* errors;
+extern atomic_bool criticalFailure;
+
+void* allocate(size_t size);
+void* reallocate(void* ptr, size_t size);
+void* freeAllocation(void* ptr);
+
+LexerState* initLexer(char* filename, char* fileContent, size_t fileSize);
+void setupKeywords();
+int lex(File *file);
 void showLex(LexerState* lexer);
 
-void macro(LexerState* lexer, Token* input, int inputCount, Token* target, int targetCount, int start);
+ParserState* initParser(char* filename, int fileSize, const char* file, Token* tokenArray, int count);
+int parse(File *file);
+void showAST(ParserState* parser, ASTNode* node, int depth);
 
-void parse();
+void throwError(char* filename, int fileSize, const char* file, TokenLocation location, ErrorType errorCode, int info, ErrorClass errorClass, ...);
 
-char* mmapInit(int fileDescriptor, size_t *fileSize);
-void throwError(char *message, int line, int column);
-char* literalRead(Token str, const char* sourceText, char* fmt);
+void macro(ParserState* parser, Token* input, int inputCount, Token* target, int targetCount, int start);
 
-File* initFile(char* filename);
+const char* fetchTokenTypeString(TokenType type);
+const char* fetchTokenStateString(StateType type);
+
+static inline void extractTokenText(ParserState* parser, Token token, char* buffer, size_t bufferSize) {
+    if (token.type == TOKEN_EOF) {
+        strncpy(buffer, "EOF", bufferSize); 
+        return;
+    }
+
+    size_t copyLength = token.pos.length < (bufferSize - 1) ? token.pos.length : (bufferSize - 1);
+
+    memcpy(buffer, parser->file + token.pos.loc, copyLength);
+    buffer[copyLength] = '\0';
+}
 
 #endif
